517667d53d22b36812b32203cf5da9d1ea4204c2
Dante Cypress <dantecypress@gmail.com>
2020-09-08 12:12:06 -0700

    firebase added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBNBOcEsI_C-1_6s9EU_7cQDNg2ZZLQDjQ",
+  authDomain: "smirk-9248a.firebaseapp.com",
+  databaseURL: "https://smirk-9248a.firebaseio.com",
+  projectId: "smirk-9248a",
+  storageBucket: "smirk-9248a.appspot.com",
+  messagingSenderId: "288387031583",
+  appId: "1:288387031583:web:e12b4a0c058df59bdd6b2a",
+  measurementId: "G-268ZVK8LQE",
+};
